b2f71abc77327156e5341885a145ac4ffe4117f8
Praveen Kumar <praveenscience@gmail.com>
2021-12-17 20:01:06 +0000

    Added a centrally aligned header & 404.

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header dark={true} className="Header">
+        <Header dark={true} className="Header justify-content-center">
           COVID Flight Experiences
         </Header>
+        <div className="text-center p-5 m-5">
+          <img src="https://i.imgur.com/BZjNB8B.png" alt="Work in Progress" />
+          <p className="display-4">Work in Progress</p>
+        </div>
       </div>
     );
   }
