b20d0ef4e91ea2cd2e7679137473f57b7efed7b7
Praveen Kumar <praveenscience@gmail.com>
2021-12-17 19:30:45 +0000

    Clean up from CRA and Reorganised files.

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const headingText = screen.getByText(/COVID Fly/i);
+  expect(headingText).toBeInTheDocument();
+});
