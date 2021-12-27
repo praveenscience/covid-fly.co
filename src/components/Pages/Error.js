bb698ae4aa3c8d22434c84d11706cef612f1e08f
Praveen Kumar <praveenscience@gmail.com>
2021-12-27 11:12:48 +0000

    Updated the Error Page.

--- src/components/Pages/Error.js
+++ src/components/Pages/Error.js
@@ -1,7 +1,10 @@
 const Error = () => {
   return (
     <div className="text-center p-5 m-5">
-      <img src="https://i.imgur.com/BZjNB8B.png" alt="Woops! An Error Occurred!" />
+      <img
+        src="https://i.imgur.com/BZjNB8B.png"
+        alt="Woops! An Error Occurred!"
+      />
       <p className="display-4">Woops! An Error Occurred!</p>
     </div>
   );
