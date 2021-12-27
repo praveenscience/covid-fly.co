793c0bfbd51df8289679234a78157ef4fc13b410
Praveen Kumar <praveenscience@gmail.com>
2021-12-27 11:40:48 +0000

    Added Textarea as option for FormGroup.

--- src/components/Bootstrap/FormGroup.js
+++ src/components/Bootstrap/FormGroup.js
@@ -13,19 +13,33 @@ const FormGroup = ({
   return (
     <div className="form-group">
       <label htmlFor={Id}>{Label}</label>
-      <input
-        type={Type}
-        className="form-control"
-        id={Id}
-        name={Id}
-        onChange={onChange}
-        value={Value}
-        placeholder={Placeholder}
-        defaultValue={defaultValue}
-        checked={Checked}
-        defaultChecked={defaultChecked}
-        aria-describedby={Id + "Help"}
-      />
+      {Type === "textarea" ? (
+        <textarea
+          className="form-control"
+          id={Id}
+          name={Id}
+          onChange={onChange}
+          value={Value}
+          placeholder={Placeholder}
+          defaultValue={defaultValue}
+          aria-describedby={Id + "Help"}
+        ></textarea>
+      ) : (
+        <input
+          type={Type}
+          className="form-control"
+          id={Id}
+          name={Id}
+          onChange={onChange}
+          value={Value}
+          placeholder={Placeholder}
+          defaultValue={defaultValue}
+          checked={Checked}
+          defaultChecked={defaultChecked}
+          aria-describedby={Id + "Help"}
+        />
+      )}
+
       {Desc && (
         <small id={Id + "Help"} className="form-text text-muted">
           {Desc}
