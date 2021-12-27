09c717678f544e653e5cbaedb5509e0c0c6a3dc9
Praveen Kumar <praveenscience@gmail.com>
2021-12-27 11:13:00 +0000

    Added the basic contents and state for the New Screen.

--- src/components/Pages/New.js
+++ src/components/Pages/New.js
@@ -1,10 +1,105 @@
 import React, { Component } from "react";
+import FormGroup from "../Bootstrap/FormGroup";
 
 class New extends Component {
+  state = {};
   render() {
+    const FormFields = [
+      {
+        Id: "Source",
+        Label: "Source",
+        Type: "text",
+        Placeholder: "Enter the Starting Airport"
+      },
+      {
+        Id: "Destination",
+        Label: "Destination",
+        Type: "text",
+        Placeholder: "Enter the Final Destination"
+      },
+      {
+        Id: "Hops",
+        Label: "Hops",
+        Type: "textarea",
+        Placeholder:
+          "List down all the different Airport Codes one by one per line."
+      },
+      {
+        Id: "Docs",
+        Label: "Documents Needed",
+        Type: "textarea",
+        Placeholder:
+          "List down all the different documents required one by one per line."
+      },
+      {
+        Id: "QuarantineInfo",
+        Label: "Quarantine Info",
+        Type: "text",
+        Placeholder:
+          "Please let us know your Quarantine Experience, whether it's Mandatory Quarantine or Home one, and for how long"
+      },
+      {
+        Id: "TravelDate",
+        Label: "Travel Date",
+        Type: "datetime",
+        Placeholder: "Enter the Date and Time when you're travelling."
+      },
+      {
+        Id: "Author",
+        Label: "Name",
+        Type: "text",
+        Placeholder: "Please enter your name."
+      },
+      {
+        Id: "Email",
+        Label: "Email",
+        Type: "text",
+        Placeholder: "Please enter your email address."
+      }
+    ];
     return (
-      <div className="App">
-        <p>This is new component</p>
+      <div className="New">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h2>Enter your Experience</h2>
+              <form>
+                {FormFields.map(fg => (
+                  <FormGroup key={fg.Id} {...fg} />
+                ))}
+              </form>
+            </div>
+            <div className="col-12 col-md-6">
+              <h2>Files to Commit</h2>
+              <h3>
+                Update <code>Experiences.json</code> Array
+              </h3>
+              <pre className="border rounded bg-light p-1">
+                {JSON.stringify(
+                  {
+                    Source: "",
+                    Destination: "",
+                    Hops: [],
+                    Docs: [],
+                    QuarantineInfo: "",
+                    TravelDate: "2021-12-15T01:20:20.000Z",
+                    Meta: {
+                      Author: "",
+                      Email: "",
+                      DatePosted: "2021-12-15T01:20:20.000Z"
+                    },
+                    Description: "",
+                    DescFile: ""
+                  },
+                  null,
+                  2
+                )}
+              </pre>
+              <h3>Create a Markdown File</h3>
+              <pre className="border rounded bg-light p-1">{`Markdown Contents`}</pre>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
